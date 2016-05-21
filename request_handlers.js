b02302d39bd08ba7302e4a279140f404a218265c
Stephen Schroeder <schrode50@gmail.com>
2016-05-20 19:24:33 -0700

    going through the examples of bad code

--- request_handlers.js
+++ request_handlers.js
@@ -1,8 +1,19 @@
+const exec = require('child_process').exec;
+
 start = () => {
   console.log("Request handler 'start' was called.");
+  var content = 'empty';
+
+  exec('find /', (err, stdout,stderr) => {
+    content = stdout;
+  });
+
+  return content;
 }
+
 upload = () => {
   console.log("Request handler 'upload' was called.");
+  return 'Hello Upload';
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -6,10 +6,9 @@ start = (route, handle) => {
     var pathname = url.parse(req.url).pathname;
     console.log('Request for ' + pathname + ' received');
 
-    route(handle, pathname);
-
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
+    var content = route(handle, pathname)
+    res.write(content);
     res.end();
   }
 
