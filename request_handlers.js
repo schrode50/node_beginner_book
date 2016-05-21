483d0bda94c925fe285feaf8f869a047d0850331
Stephen Schroeder <schrode50@gmail.com>
2016-05-20 18:27:31 -0700

    finally figured out a stupid mistak that wasnt necessarily mine

---
+++ request_handlers.js
@@ -0,0 +1,9 @@
+start = () => {
+  console.log("Request handler 'start' was called.");
+}
+upload = () => {
+  console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require('http');
+var url = require('url');
 
-onRequest = (req, res) => {
-  console.log('Request received');
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.write('Hello World');
-  res.end();
+start = (route, handle) => {
+  onRequest = (req, res) => {
+    var pathname = url.parse(req.url).pathname;
+    console.log('Request for ' + pathname + ' received');
+
+    route(handle, pathname);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
